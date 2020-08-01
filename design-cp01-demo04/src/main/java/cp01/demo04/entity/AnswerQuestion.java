b5e32e4d96570293845165d95e710579e59ab7fc
kiragoo <zhangl910201@163.com>
2020-08-01 13:50:03 +0800

    【ADD】 新增创建者模式之原型模式

---
+++ design-cp01-demo04/src/main/java/cp01/demo04/entity/AnswerQuestion.java
@@ -0,0 +1,30 @@
+package cp01.demo04.entity;
+
+public class AnswerQuestion {
+    private String name;  // 问题
+    private String key;   // 答案
+
+    public AnswerQuestion() {
+    }
+
+    public AnswerQuestion(String name, String key) {
+        this.name = name;
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
