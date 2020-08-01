913767309996a27269e90c169d75e0b9d0bed44a
kiragoo <zhangl910201@163.com>
2020-08-01 13:50:17 +0800

    【ADD】 新增创建者模式之原型模式

---
+++ design-cp01-demo04/src/test/java/cp01/demo04/QuestionBankControllerTest.java
@@ -0,0 +1,15 @@
+package cp01.demo04;
+
+import org.junit.Test;
+
+
+public class QuestionBankControllerTest {
+    
+    @Test
+    public void testQuestionBankController() throws CloneNotSupportedException {
+        QuestionBankController questionBankController = new QuestionBankController();
+        System.out.println(questionBankController.createPaper("花花", "1000001921032"));
+        System.out.println(questionBankController.createPaper("豆豆", "1000001921051"));
+        System.out.println(questionBankController.createPaper("大宝", "1000001921987"));
+    }
+}
